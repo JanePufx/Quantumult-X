036d8d5cc67fd43b4b8eaafd9f81bf47ca65df3d
JanePufx <99172676+JanePufx@users.noreply.github.com>
2023-06-21 17:08:55 +0800

    Update and rename 菜鸟.js to 菜鸟去广告.js

--- 菜鸟去广告.js
+++ 菜鸟去广告.js
@@ -3,12 +3,12 @@
 # 样例可参见 https://raw.githubusercontent.com/crossutility/Quantumult-X/master/sample-import-rewrite.snippet
  
  ## 菜鸟              
-hostname = amdc.m.taobao.com, cn-acs.m.cainiao.com
+hostname = amdc.m。taobao。com, cn-acs.m。cainiao。com
 ;^http:\/\/amdc\.m\.taobao\.com\/amdc\/mobileDispatch$ Cainiao4iPhone url-and-header reject-dict
 ## 菜鸟首页          今日好物推荐,底部商品推广,底部标签页,问卷调查,主页图标 //cn-acs.m.cainiao.com
 ^https:\/\/cn-acs\.m\.cainiao\.com\/gw\/mtop\.cainiao\.adkeyword\.get\.cn\/1\.0\? url reject-dict
 ^https:\/\/cn-acs\.m\.cainiao\.com\/gw\/mtop\.cainiao\.guoguo\.nbnetflow\.ads\.index\.cn\/1\.0\? url reject-dict
-^https:\/\/cn-acs\.m\.cainiao\.com\/gw\/mtop\.cainiao\.guoguo\.nbnetflow\.ads\.m?show\.cn\/1\.0\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/cainiao.js
+^https:\/\/cn-acs\.m\.cainiao\.com\/gw\/mtop\.cainiao\.guoguo\.nbnetflow\.ads\.m?show\.cn\/1\.0\? url script-response-body https://raw.githubusercontent。com/RuCu6/QuanX/main/Scripts/cainiao.js
 ^https:\/\/cn-acs\.m\.cainiao\.com\/gw\/mtop\.cainiao\.nbmensa\.research\.researchservice\.(acquire|event|close)\.cn\/1\.0\? url reject-dict
 
 
