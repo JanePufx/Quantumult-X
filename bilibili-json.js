825d8d0438b55f15e1bef53dfb8d40502e4628c5
JanePufx <2414042841@qq.com>
2023-08-14 16:50:46 +0800

    Update bilibili-json.js

--- bilibili-json.js
+++ bilibili-json.js
@@ -67,26 +67,6 @@ if (!body.data) {
             $notification.post(notifyTitle, '推荐页', "items字段错误");
         } else {
             body.data.items = body.data.items.filter(i => {
-
-                console.log(`up:${i.args.up_name}`);
-                // 屏蔽up uid方式
-                const up_uid = ['6574487', '450882837', '37119626', '476993587', '3493277586164423', '385105236', '13972445', '43707221', '390461123', '630721866', '6087825', '1428923', '170801610', '317839021', '602664449', '443462760', '255508156', '1213489275', '204408558', '1985025', '314298412', '251569233', '1939319', '382953284', '163637592', '446430908', '8366990', '1192648858'];
-                if(!i.args){
-                  // console.log(`body:${$response.body}`);
-                }  else if (up_uid.includes(i.args.up_id)) {
-                  console.log(`屏蔽up:${i.args.up_name}`);
-                  return false;
-                }
-                
-                // 屏蔽视频 关键字方式
-                const key_word = ['TFBOYS','波奇','孤独'];
-                if(!i.title){
-                  console.log(`body:${$response.body}`);
-                }  else if (key_word.includes(i.title)) {
-                  console.log(`屏蔽视频:${i.title}`);
-                  return false;
-                }
-
                 const {card_type: cardType, card_goto: cardGoto} = i;
                 if (cardType && cardGoto) {
                     if (cardType === 'banner_v8' && cardGoto === 'banner') {
@@ -121,6 +101,28 @@ if (!body.data) {
                     console.log(`body:${$response.body}`);
                     $notification.post(notifyTitle, '推荐页', "无card_type/card_goto");
                 }
+
+
+                console.log(`up:${i.args.up_name}`);
+                // 屏蔽up uid方式
+                const up_uid = ['6574487', '450882837', '37119626', '476993587', '3493277586164423', '385105236', '13972445', '43707221', '390461123', '630721866', '6087825', '1428923', '170801610', '317839021', '602664449', '443462760', '255508156', '1213489275', '204408558', '1985025', '314298412', '251569233', '1939319', '382953284', '163637592', '446430908', '8366990', '1192648858'];
+                if(!i.args){
+                  // console.log(`body:${$response.body}`);
+                }  else if (up_uid.includes(i.args.up_id)) {
+                  console.log(`屏蔽up:${i.args.up_name}`);
+                  return false;
+                }
+                
+                // 屏蔽视频 关键字方式
+                const key_word = ['TFBOYS','波奇','孤独'];
+                if(!i.title){
+                  console.log(`body:${$response.body}`);
+                }  else if (key_word.includes(i.title)) {
+                  console.log(`屏蔽视频:${i.title}`);
+                  return false;
+                }
+
+
                 return true;
             });
         }
