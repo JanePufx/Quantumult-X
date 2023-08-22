32ef1c4fc121e0d289335e097ba999226d0d6f01
JanePufx <2414042841@qq.com>
2023-08-22 23:27:11 +0800

    Update ujing.js

--- ujing.js
+++ ujing.js
@@ -18,7 +18,7 @@ if (!body.data) {
         if (!body.data.admin) {
             console.log('数据无body.data.admin字段');
         } else {
-            body.data.admin = body.data.admin.filter(i => {
+            body.data.home = body.data.home.filter(i => {
                 if(i.name === "Ujing - iOS - 首页 - 福利中心"){
                     console.log('去除Ujing - iOS - 首页 - 福利中心');
                     return false;
