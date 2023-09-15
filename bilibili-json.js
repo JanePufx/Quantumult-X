6a5e43320abe430cb7775f8f2961ae46327cf302
JanePufx <2414042841@qq.com>
2023-09-15 22:51:02 +0800

    Update bilibili-json.js

--- bilibili-json.js
+++ bilibili-json.js
@@ -164,11 +164,9 @@ if (url.includes("/x/resource/show/skin")) {
             // Filter out items with any of the specified keywords in the title or specified up_id values
             console.log(`\n屏蔽视频title:${title}\n屏蔽视频作者:${up_name}\n屏蔽视频作者uid:${up_id}\n屏蔽视频播放量:${video}\n屏蔽视频弹幕:${pop}`);
             return false;
-          } else if (!rcmd_reason_style){
-            if (['竖屏'].includes(text)) {
+          } else if (['竖屏'].includes(text)) {
               console.log(`\n屏蔽竖版视频title:${title}\n屏蔽竖版视频作者:${up_name}\n屏蔽竖版视频作者uid:${up_id}\n屏蔽竖版视频播放量:${video}\n屏蔽竖版视频弹幕:${pop}\n屏蔽竖版视频version:${text}`);
               return false;
-            }
           } else if (Number(pop) <= 100) {
             console.log(`\n屏蔽视频title:${title}\n屏蔽视频作者:${up_name}\n屏蔽视频作者uid:${up_id}\n屏蔽视频播放量:${video}\n屏蔽视频弹幕:${pop}\n---弹幕过少---`);
             return false;
